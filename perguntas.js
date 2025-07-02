f1c5eb64848a3d14afd38e6fdb52ed46a0c3828d
BrunoGuerrien <166255932+BrunoGuerrien@users.noreply.github.com>
2025-07-02 08:45:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação', //Tema do cartão  
+    'O que é Python?', // Adicionar nova pergunta 
+    'O Python é uma linguagem de programação'  // Adicionar uma nova resposta à pergunta anterior
+)
+
+criaCartao(
+    'Geografia', //Tema do cartão  
+    'Qual a capital da França?', // Adicionar nova pergunta 
+    'A capital da França é Paris' // Adicionar uma nova resposta à pergunta anterior
+)
+
+criaCartao(
+    'Programação', //Tema do cartão  
+    'O que é uma função?', // Adicionar nova pergunta 
+    'Uma função é um bloco de código que executa alguma tarefa' // Adicionar uma nova resposta à pergunta anterior
+)
+
+criaCartao(
+    'Lingua inglesa', //Tema do cartão  
+    'Como se diz oi em Inglês?', // Adicionar nova pergunta 
+    'Oi em ingles é HI (RAI)' // Adicionar uma nova resposta à pergunta anterior
+)
